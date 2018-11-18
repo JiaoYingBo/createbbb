c82b9079c517a2b43ab15cf2e142622d71dd6421
JiaoYingBo <715775799@qq.com>
2018-11-18 18:46:49 +0800

    555

---
+++ GraduateProj/GraduateProj/GPExpand/ResponseModel/GPBaseResponseModel.m
@@ -0,0 +1,13 @@
+//
+//  GPBaseResponseModel.m
+//  GraduateProj
+//
+//  Created by 焦英博 on 2018/11/18.
+//  Copyright © 2018年 mlg. All rights reserved.
+//
+
+#import "GPBaseResponseModel.h"
+
+@implementation GPBaseResponseModel
+
+@end
